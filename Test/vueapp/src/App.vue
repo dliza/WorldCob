40ce7a7dd88a4f3b9fbe3c0544e2ba71cf124444
dliza <102047464+dliza@users.noreply.github.com>
2022-05-17 00:36:23 -0500

    New Vue App in Test

---
+++ Test/vueapp/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <OneComponent />
+    <TwoComponent />
+  </div>
+</template>
+
+<script>
+import OneComponent from "./components/OneComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    OneComponent,
+  },
+  data: () => ({}),
+  mounted() {},
+};
+</script>
+
+<style>
+body {
+  background: #ededed;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Test/vueapp/src/components/OneComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <TwitterButton
+      url="https://bizz-dome-uploads--demo.s3.amazonaws.com/media/2022/03/blob-45085_n-500x500.png"
+      description="Descripción para el Tweet"
+      :isBlank="false"
+      btnText
+    />
+    <FacebookButton
+      url="https://bizz-dome-uploads--demo.s3.amazonaws.com/media/2022/03/blob-45085_n-500x500.png"
+      :isBlank="false"
+      btnText
+    />
+    <LinkedInButton
+      url="https://bizz-dome-uploads--demo.s3.amazonaws.com/media/2022/03/blob-45085_n-500x500.png"
+      :isBlank="false"
+      btnText
+    />
+  </div>
+</template>
+
+<script>
+// Vue Share Component
+import TwitterButton from "vue-share-buttons/src/components/TwitterButton.vue";
+import FacebookButton from "vue-share-buttons/src/components/FacebookButton.vue";
+import LinkedInButton from "vue-share-buttons/src/components/LinkedInButton.vue";
+
+export default {
+  name: "OneComponent",
+  components: {
+    TwitterButton,
+    FacebookButton,
+    LinkedInButton,
+  },
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
