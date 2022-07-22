d32fda212ce9b0e756deb8f34d1587cf79663670
dliza <102047464+dliza@users.noreply.github.com>
2022-07-22 02:22:30 -0500

    todo

--- Test/vueapp/src/App.vue
+++ Test/vueapp/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <OneComponent />
-    <TwoComponent />
   </div>
 </template>
 
--- Test/vueapp/src/main.js
+++ Test/vueapp/src/main.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueHtmlToPaper from 'vue-html-to-paper';
+// const options = {
+//   name: '_blank',
+//   specs: [
+//     'fullscreen=yes',
+//     'titlebar=yes',
+//     'scrollbars=yes'
+//   ],
+//   styles: [
+//     'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+//     'https://unpkg.com/kidlat-css/css/kidlat.css'
+//   ],
+//   timeout: 1000, // default timeout before the print window appears
+//   autoClose: true, // if false, the window will not close after printing
+//   windowTitle: window.document.title, // override the window title
+// }
+
+Vue.use(VueHtmlToPaper);
 
 Vue.config.productionTip = false
 
